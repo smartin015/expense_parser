81e12198c7420315df0684d62f34f61843267002
Scott Martin <smartin015@gmail.com>
2024-12-27 15:11:22 -0500

    Add argparse and logging to expense parser, also extend collection window and keep a filter of last month's expenses to remove

--- expense_parser/parse_expenses.py
+++ expense_parser/parse_expenses.py
@@ -3,28 +3,23 @@ import os
 import sys
 import re
 import yaml
-from dateutil import parser
-from dateutil.relativedelta import relativedelta
-from datetime import datetime
+import argparse
+import logging
+from dateutil import parser as dateparser
+import datetime
+from collections import defaultdict
 
-now = datetime.now()
-cur_month = now.month
-interval_start = now.replace(day=27, hour=0, minute=0, second=0)
-if interval_start < now:
-    interval_end = interval_start
-else:
-    interval_end = interval_start - relativedelta(months=1)
-
-interval_start = interval_end - relativedelta(months=1)
-
-sys.stderr.write(f"Printing transactions from {interval_start} to {interval_end}\n")
+logging.basicConfig(level=logging.INFO)
+log = logging.getLogger("main")
 
 class TxnPrinter:
-    def __init__(self, date_idx, desc_idx, category_idx, debit_idx, negative_debit=False):
+    def __init__(self, name, date_idx, desc_idx, category_idx, debit_idx, negative_debit=False, prev_expenses={}):
+        self.name = name
         self.date_idx = date_idx
         self.desc_idx = desc_idx
         self.category_idx = category_idx
         self.debit_idx = debit_idx
+        self.prev_lut = prev_expenses
         self.matchers = []
         self.negative_debit = negative_debit
 
@@ -40,9 +35,10 @@ class TxnPrinter:
             field, category, regex_str = line.split(maxsplit=2)
             self.matchers.append((field, re.compile(regex_str), category))
 
-    def printRow(self, row):
-        date = parser.parse(row[self.date_idx])
+    def printRow(self, row, interval_start, interval_end):
+        date = dateparser.parse(row[self.date_idx])
         if (date > interval_end) or (date < interval_start):
+            log.info(f"Omit - {date} > {interval_end} or < {interval_start}")
             return False
         if row[self.debit_idx].strip() == "":
             return False
@@ -50,6 +46,7 @@ class TxnPrinter:
         if self.negative_debit:
             debit = -debit
         if abs(debit) < 0.01:
+            log.info(f"Omitting negligible charge: {row[self.desc_idx]} (${str(debit)})")
             return False
 
         if self.category_idx:
@@ -65,56 +62,90 @@ class TxnPrinter:
                 category = cat
                 break
         if category == "OMIT":
-            sys.stderr.write(f"Omitting {row[self.desc_idx]} (${str(debit)})\n")
+            log.warning(f"Omitting {row[self.desc_idx]} (${str(debit)})")
             return False
 
         desc = row[self.desc_idx].replace("\"", "")
-        print(f"{date.strftime('%Y-%m-%d')}, \"{desc}\", {category}, {debit}")
+        out = f"{date.strftime('%Y-%m-%d')}, \"{desc}\", {category}, {debit}"
+        if out in self.prev_lut:
+            log.warning(f"Omit previously exported row: {out}")
+            return False
+        print(out)
         return True
 
-filestats = {}
-for path in sys.argv[1:]:
-    filestats[path] = 0
-    with open(path, encoding='latin1') as f:
-        sys.stderr.write(f"Reading {path}\n")
-        data = f.read().split('\n')
-    
-    if data[0].startswith("Note:"):
-        header_idx = 2
-    else:
-        header_idx = 0
+def parse_expenses(prev, paths, interval_start, interval_end):
+    filestats = defaultdict(int)
 
-    with open(os.path.join(os.path.dirname(__file__), "./config/manifest.yaml")) as f:
-      manifest = yaml.safe_load(f.read())
+    # Get all prior lines so we can ensure we're not duplicating anything
+    with open(prev, encoding="utf8") as f:
+        prev_lut = {l for l in f.read().split("\n")}
+    log.info(f"Loaded {len(prev_lut)} previous entries to avoid")
 
-    hdr = dict([(v,i) for (i, v) in enumerate(data[header_idx].split(","))])
-    printer = None
-    for name, cfg in manifest.items():
-      if cfg['match'] in hdr.keys():
-          sys.stderr.write(f'Match on parser {name}\n')
-          if cfg.get('pass', False):
+    for path in paths:
+        if path.endswith("out.csv"):
+            log.warning(f"Skipping likely-dest-output out.csv file at {path}")
             continue
-          # 'Transaction Date', 'Posted Date', 'Card No.', 'Description', 'Category', 'Debit', 'Credit'
-          printer = TxnPrinter(
+
+        with open(path, encoding='latin1') as f:
+            log.info(f"Reading {path}")
+            data = f.read().split('\n')
+
+        if data[0].startswith("Note:"):
+            header_idx = 2
+        else:
+            header_idx = 0
+
+        with open(os.path.join(os.path.dirname(__file__), "./config/manifest.yaml")) as f:
+          manifest = yaml.safe_load(f.read())
+
+        hdr = dict([(v,i) for (i, v) in enumerate(data[header_idx].split(","))])
+        printer = None
+        for name, cfg in manifest.items():
+          if cfg['match'] in hdr.keys():
+              log.info(f'Match on parser {name}')
+              if cfg.get('pass', False):
+                continue
+              # 'Transaction Date', 'Posted Date', 'Card No.', 'Description', 'Category', 'Debit', 'Credit'
+              printer = TxnPrinter(
+                  name,
                   date_idx = hdr[cfg['hdr']['date']],
                   desc_idx = hdr[cfg['hdr']['desc']],
                   category_idx = hdr.get(cfg['hdr']['category']),
                   debit_idx = hdr[cfg['hdr']['debit']],
                   negative_debit = cfg['negative_debit'],
-                  )
-          printer.loadCategoryMap(os.path.join(os.path.dirname(__file__), f"./config/{cfg['rules']}"))
-          break
+                  prev_expenses = prev_lut,
+              )
+              printer.loadCategoryMap(os.path.join(os.path.dirname(__file__), f"./config/{cfg['rules']}"))
+              break
 
-    if printer is None:
-        sys.stderr.write(f"ERROR: origin of file {path} not resolved")
-        sys.exit(1)
+        if printer is None:
+            log.error(f"ERROR: origin of file {path} not resolved")
+            sys.exit(1)
 
-    for row in csv.reader(data[header_idx+1:]):
-        if len(row) == 0:
-            continue
-        printed = printer.printRow(row)
-        filestats[path] += 1 if printed else 0
+        for row in csv.reader(data[header_idx+1:]):
+            if len(row) == 0:
+                continue
+            printed = printer.printRow(row, interval_start, interval_end)
+            filestats[f"({printer.name})\t {path}"] += 1 if printed else 0
+    return filestats
 
-sys.stderr.write(f"\n\n===================\nParsed {len(sys.argv[1:])} files:\n")
-for k,v in filestats.items():
-    sys.stderr.write(f"{k.ljust(40)}\t{v} items parsed\n")
+if __name__ == "__main__":
+    ap = argparse.ArgumentParser(description="Parse credit card and other statements to produced an organized transaction list")
+    ap.add_argument("--prev", type=str, required=True, help="Path to previous output file, for deduplication")
+    ap.add_argument('--paths', nargs='+', type=str, required=True, help='File paths to read')
+    args = ap.parse_args(sys.argv[1:])
+
+    # Interval a little bit before and after the month duration, to account for overlap
+    interval_end = datetime.datetime.now().replace(hour=0, minute=0, second=0, microsecond=0)
+    while interval_end.day != 27:
+        interval_end += datetime.timedelta(days=3)
+    interval_start = interval_end - datetime.timedelta(days=30 + 7)
+
+    log.info(f"Printing transactions from {interval_start} to {interval_end}, excluding any already listed in {args.prev}")
+
+    filestats = parse_expenses(args.prev, args.paths, interval_start, interval_end)
+
+    log.info(f"===================")
+    log.info("Parsed {len(args.paths)} files:")
+    for k,v in filestats.items():
+        log.info(f"{k.ljust(40)}\t{v} item(s) parsed")
---
+++ expense_parser/shell.nix
@@ -0,0 +1,11 @@
+let
+  pkgs = import <nixpkgs> {};
+in pkgs.mkShell {
+  packages = [
+    (pkgs.python3.withPackages (python-pkgs: with python-pkgs; [
+        pip
+        pyyaml
+        dateutil
+    ]))
+  ];
+}
